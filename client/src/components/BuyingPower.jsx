ef18395bdbaa88862bc96452f9e880be514054ce
Jingshi Liu <jingshiliu07@gmail.com>
2022-11-04 22:29:17 -0400

    commit to get updated code from master

--- client/src/components/BuyingPower.jsx
+++ client/src/components/BuyingPower.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import '../CSS/BuyingPower.css'
+import React, {useState} from 'react';
 
-function BuyingPower() {
+function BuyingPower({buyingPower}) {
+    const [isAddingBuyingPower, setIsAddingBuyingPower] = useState(false);
+
+    buyingPower = 10000;
+    let buyingUI;
+    if(isAddingBuyingPower){
+        buyingUI = (
+            <form className='BuyingPower__add-buying-power'>
+                <div>
+                    <span>Add Power! </span>
+                    <input type="text" placeholder='Enter amount and pay nothing'/>
+                </div>
+                <button onClick={(e)=>{
+                    e.preventDefault()
+                    // get
+                    // Send an api request to backend to add money
+                    // /buying-power
+                    // put(add) amount
+                }}>
+                    Confirm
+                </button>
+            </form>)
+    }
+
     return (
         <section className='BuyingPower'>
-
+            <div className="BuyingPower__container">
+                <div className="BuyingPower__header"
+                    onClick={()=>{
+                        setIsAddingBuyingPower(!isAddingBuyingPower)
+                    }}>
+                    <span>Buying Power: </span>
+                    <span>${buyingPower}</span>
+                </div>
+                {buyingUI}
+            </div>
         </section>
     );
 }
