c341428d3a19d3dcc8ed39386a849183a3ebfc00
Jingshi Liu <jingshiliu07@gmail.com>
2022-11-14 22:52:27 -0500

    removed RecommendingStock

--- client/src/components/MainAppContent.jsx
+++ client/src/components/MainAppContent.jsx
@@ -4,8 +4,9 @@ import BuyingPower from "./BuyingPower";
 import Options from "./Options";
 import TrendingStocks from "./TrendingStocks";
 import PortfolioStats from "./PortfolioStats";
+import News from "./News";
 
-function MainAppContent({stockCandles}) {
+function MainAppContent({stockCandles, news}) {
     return (
         <main className='MainAppContent'>
             <div className="MainBodyContent__column">
@@ -13,13 +14,13 @@ function MainAppContent({stockCandles}) {
                 <PortfolioStats stockCandles={stockCandles}/>
             </div>
 
-            <div className="MainBodyContent__column">
-                <Options />
-                <TrendingStocks stockCandles={stockCandles} />
-            </div>
+            {/*<div className="MainBodyContent__column">*/}
+            {/*    <Options />*/}
+            {/*    <TrendingStocks stockCandles={stockCandles} />*/}
+            {/*</div>*/}
 
             <div className="MainBodyContent__column">
-                {/*<News />*/}
+                <News news={news}/>
             </div>
         </main>
     );
