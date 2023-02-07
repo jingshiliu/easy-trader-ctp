c042d18bc02546dfaae09f2c629e99f5d4002306
Jingshi Liu <jingshiliu07@gmail.com>
2023-02-06 19:19:34 -0500

    added a route '/firebaseVerifyToken' and a middleware sessionValidator

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+const cookieParser = require('cookie-parser')
+const {firebaseVerifyToken, sessionValidator} = require('./Authenticator')
+
 const UserStockRouter = require('./routes/UserStockRouter')
 const StockRouter = require('./routes/StockRouter')
 const NewsRouter = require('./routes/NewsRouter')
@@ -9,12 +12,16 @@ const port = process.env.PORT || 8001
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(cookieParser())
+app.use(sessionValidator)
 
 app.use('/userstock', UserStockRouter)
 
 app.use('/stock', StockRouter)
 
 app.use('/news', NewsRouter)
+
+app.post('/firebaseVerifyToken', firebaseVerifyToken)
 
 app.listen(port, ()=>{
     console.log(`Running on ${port}`)
