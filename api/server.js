9446e07b98dd2bd622d516ec071b15fca1986869
Jingshi Liu <jingshiliu07@gmail.com>
2023-02-06 19:24:07 -0500

    added back sessionValidator so all user needs to be signed in to use our api

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const cookieParser = require('cookie-parser')
-const {firebaseVerifyToken} = require('./Authenticator')
+const {firebaseVerifyToken, sessionValidator} = require('./Authenticator')
 
 const UserStockRouter = require('./routes/UserStockRouter')
 const StockRouter = require('./routes/StockRouter')
@@ -13,6 +13,7 @@ const port = process.env.PORT || 8001
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cookieParser())
+app.use(sessionValidator)
 
 app.use('/userstock', UserStockRouter)
 
